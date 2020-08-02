20d14a57d7c193298a03aef6c6949c8112c7e973
lwpearce <lwpearce@yahoo.com>
2020-08-01 21:44:51 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';  
+import { SummaryData, CountryData } from './models';  
+import { DataService } from './data.service';  
+import { DatePipe } from '@angular/common';  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css'],  
+  providers: [DatePipe]  
+})  
+export class AppComponent implements OnInit {  
+  title = 'covid19-tracker';  
+  summaryData: SummaryData;  
+  indiaData: CountryData;  
+  usaData: CountryData;
+  selectedCountryData: CountryData;  
+  highlyConfirmedData: Array<CountryData>;  
+  highlyDeathData: Array<CountryData>;  
+  highlyRecoveredData: Array<CountryData>;  
+  currentDate: string;  
+  
+  constructor(private service: DataService, private datePipe: DatePipe) { }  
+  
+  ngOnInit() {  
+    let date = new Date();  
+    this.currentDate = this.datePipe.transform(date,'dd-MMM-yyyy');  
+    this.getAllData();  
+  }  
+  
+  getAllData() {  
+    this.service.getData().subscribe(  
+      response => {  
+        this.summaryData = response;  
+        this.getIndiaData();  
+        this.getSortedData();  
+      }  
+    )  
+  }  
+
+  getUSAData() {
+    this.usaData = this.summaryData.Countries.find(x => x.Slug == "united-states");
+  }
+  
+  getIndiaData() {  
+    this.indiaData = this.summaryData.Countries.find(x => x.Slug == "india");  
+  }  
+  
+  getSortedData() {  
+    let data = JSON.parse(JSON.stringify(this.summaryData.Countries));  
+    this.highlyConfirmedData = data.sort((a, b) => b.TotalConfirmed - a.TotalConfirmed).slice(0, 10);  
+    this.highlyDeathData = data.sort((a, b) => b.TotalDeaths - a.TotalDeaths).slice(0, 10);  
+    this.highlyRecoveredData = data.sort((a, b) => b.TotalRecovered - a.TotalRecovered).slice(0, 10);  
+  }  
+} 
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule, MatCard } from '@angular/material/card';
+import { MatSelectModule, MatSelect } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
